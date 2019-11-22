f13ec70081b405f535add7d133a57b2f492e8acb
Yoann Buzenet <ybuzenet@gmail.com>
2019-11-22 10:46:13 +0100

    finally managed to setup css animation

--- src/components/main-button.js
+++ src/components/main-button.js
@@ -21,13 +21,37 @@ class MainButton extends Component{
         });
     }
 
+    createAndRemoveGraphicEffect(textToRender){
+        var ul = document.getElementById('ul-main-number');
+        var li = document.createElement('li');
+        li.classList.add('graphInput');
+        li.innerText = textToRender;
+        ul.appendChild(li);
+
+        setTimeout(()=> (li.remove()), 800)
+    }
+
+    componentDidMount(){
+        var mainButton = document.getElementById('main-button-id');
+        mainButton.addEventListener('click', ()=>{
+            this.createAndRemoveGraphicEffect(this.props.productivity);
+        });
+    }
+    
+    componentWillUnmount(){
+        document.removeEventListener('click', ()=>{
+        this.createAndRemoveGraphicEffect(this.props.productivity);
+        })
+    }
+
+
     render(){
         const graphInputPaperclips = this.state.list.map(element => 
             <li className="graphInput" key={Math.random()}> {element} </li>);
 
     return (
-    <button onClick={this.increaseStockAndUpdateLocalArray} className="main-button">
-        Create <ul className="main-button-number">
+    <button onClick={this.increaseStockAndUpdateLocalArray} className="main-button" id="main-button-id">
+        Create <ul className="main-button-number" id="ul-main-number">
             <li>{this.props.productivity}</li>
             {/* {graphInputPaperclips.length > 0 ? graphInputPaperclips[graphInputPaperclips.length -1] : null} */}
             </ul> paperclips
