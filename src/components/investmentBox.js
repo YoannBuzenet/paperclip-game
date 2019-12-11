800147c638c5188ded3686b023d2f02650b7c5c0
Yoann <ybuzenet@gmail.com>
2019-12-11 19:32:55 +0100

    Colors OK. Style Nearly OK

--- src/components/investmentBox.js
+++ src/components/investmentBox.js
@@ -14,18 +14,18 @@ class InvestmentBox extends Component{
       
         // R&D button disabling
         if(this.props.rdLevelOfInvestment > 5){
-            var classNameRDButton = "rd-button menu-button doNOTdisplay";
+            var classNameRDButton = "rd-button menu-button investment-button doNOTdisplay";
         }
         else{
-            var classNameRDButton = "rd-button menu-button";
+            var classNameRDButton = "rd-button investment-button menu-button";
         }
 
         //Automatic Machine button disabling
         if(this.props.numberOfSmallAutomaticMachines >= this.props.maximumSmallAutomaticMachine){
-            var classNameAutomaticMachine = "automatic-machines-button menu-button disabled";
+            var classNameAutomaticMachine = "automatic-machines-button investment-button menu-button disabled";
         }
         else{
-            classNameAutomaticMachine = "automatic-machines-button menu-button";
+            classNameAutomaticMachine = "automatic-machines-button investment-button menu-button";
         }
 
          // Find News Ways to Make Paperclip disabling
@@ -294,7 +294,7 @@ class InvestmentBox extends Component{
         <div>
         <h3>MACHINES</h3>
             <div>
-                <Button onClickProp={this.props.buyAMachine} money={this.props.money} machineProductivity={3} picture="./pictures/small-machine.png" classNameButton="small-machines-button menu-button" cost={5} classNameChild="info-small-machines-button" contentChild="Make some more (+ 3 paperclips per click)" isManual={true} machineQuantity={1} name="Small Machine" machineType="smallMachine" createAndRemoveGraphicEffect={this.props.createAndRemoveGraphicEffect} containsAnimation={true} id='small-Machine-Button' textAnimation="+3" animationIdTarget='ul-main-number' animationTypeOfElementCreated='li' animationCSSClass='graphInputOrange'/>
+                <Button onClickProp={this.props.buyAMachine} money={this.props.money} machineProductivity={3} picture="./pictures/small-machine.png" classNameButton="small-machines-button menu-button investment-button" cost={5} classNameChild="info-small-machines-button" contentChild="Make some more (+ 3 paperclips per click)" isManual={true} machineQuantity={1} name="Small Machine" machineType="smallMachine" createAndRemoveGraphicEffect={this.props.createAndRemoveGraphicEffect} containsAnimation={true} id='small-Machine-Button' textAnimation="+3" animationIdTarget='ul-main-number' animationTypeOfElementCreated='li' animationCSSClass='graphInputOrange'/>
             </div>
         </div> 
 
@@ -303,7 +303,7 @@ class InvestmentBox extends Component{
         <div>
             <ButtonAutomaticData onClickProp={this.props.buyAMachine} money={this.props.money} picture="./pictures/automatic-machine.png" name="Automatic Machine" classNameButton={classNameAutomaticMachine} cost={this.props.smallAutomaticMachineCost} classNameChild="info-automatic-machines-button" contentChild=" paperclips per second)" automaticProductivityPerMachine={this.props.productivyPerAutomaticMachine} isManual={false} machineQuantity={1} machineType="smallAutomaticMachine" machineProductivity={this.props.smallAutomaticMachineProductivity} numberOfSmallAutomaticMachines={this.props.numberOfSmallAutomaticMachines} maximumSmallAutomaticMachine={this.props.maximumSmallAutomaticMachine}/>
         {this.props.rdLevelOfInvestment > 3 ? 
-        <ButtonAutomaticData onClickProp={this.props.buyAMachine} money={this.props.money} picture="./pictures/automatic-machinex5.png" name="5 Automatic Machine" classNameButton={classNameAutomaticMachine} cost={50} classNameChild="info-automatic-machinesx5-button" contentChild=" paperclips per second)" automaticProductivityPerMachine={this.props.productivyPerAutomaticMachine} machineQuantity={5} isManual={false} machineType="smallAutomaticMachine" machineProductivity={this.props.smallAutomaticMachineProductivity} numberOfSmallAutomaticMachines={this.props.numberOfSmallAutomaticMachines} maximumSmallAutomaticMachine={this.props.maximumSmallAutomaticMachine}/>
+        <ButtonAutomaticData onClickProp={this.props.buyAMachine} money={this.props.money} picture="./pictures/x5.png" name="5 Automatic Machine" classNameButton={classNameAutomaticMachine} cost={50} classNameChild="info-automatic-machinesx5-button" contentChild=" paperclips per second)" automaticProductivityPerMachine={this.props.productivyPerAutomaticMachine} machineQuantity={5} isManual={false} machineType="smallAutomaticMachine" machineProductivity={this.props.smallAutomaticMachineProductivity} numberOfSmallAutomaticMachines={this.props.numberOfSmallAutomaticMachines} maximumSmallAutomaticMachine={this.props.maximumSmallAutomaticMachine}/>
     : null}
     </div> 
     </div>
@@ -313,41 +313,41 @@ class InvestmentBox extends Component{
     {this.props.numberOfSmallMachines > 0 ?
         <div>
             <h3 className="sales-title">SALES</h3>
-    <Button onClickProp={this.props.investInSales} name="Call a new customer" picture="./pictures/euros_symbol.png" classNameButton="invest-marketing-button menu-button" cost={this.props.salesCost} classNameChild="info-invest-marketing-button" contentChild="+5 future sales. Take your phone and call !" createAndRemoveGraphicEffect={this.props.createAndRemoveGraphicEffect} money={this.props.money} containsAnimation={true} id='sales-button' textAnimation="+ 5" animationIdTarget='sell-area' animationTypeOfElementCreated='li' animationCSSClass='graphInputBig'/>
+    <Button onClickProp={this.props.investInSales} name="Call a new customer" picture="./pictures/euros_symbol.png" classNameButton="invest-marketing-button menu-button investment-button" cost={this.props.salesCost} classNameChild="info-invest-marketing-button" contentChild="+5 future sales. Take your phone and call !" createAndRemoveGraphicEffect={this.props.createAndRemoveGraphicEffect} money={this.props.money} containsAnimation={true} id='sales-button' textAnimation="+ 5" animationIdTarget='sell-area' animationTypeOfElementCreated='li' animationCSSClass='graphInputBig'/>
         </div>
     : null}
     {(this.props.rdLevelOfInvestment > 2 && this.props.numberOfSmallAutomaticMachines > 3)?    
-        <Button onClickProp={this.props.hireASalesman} name="Hire a salesman" picture="./pictures/salesman.png" classNameButton="salesman-button menu-button" cost={this.props.salesmanCost} classNameChild="info-salesman-button" contentChild="Develop your sales (+10 sales per second)" money={this.props.money} maximumSalesHirable={this.props.maximumSalesHirable} salesman={this.props.salesman}/>
+        <Button onClickProp={this.props.hireASalesman} name="Hire a salesman" picture="./pictures/salesman.png" classNameButton="salesman-button menu-button investment-button" cost={this.props.salesmanCost} classNameChild="info-salesman-button" contentChild="Develop your sales (+10 sales per second)" money={this.props.money} maximumSalesHirable={this.props.maximumSalesHirable} salesman={this.props.salesman}/>
     : null}
     {this.props.rdLevelOfInvestment > 3 ? 
-        <Button onClickProp={this.props.buyFiveSales} picture="./pictures/salesmanx5.png" name="Hire 5 salesmen" classNameButton="salesman-button menu-button" cost={this.props.salesmanCost *5 } classNameChild="salesman-machines-button" contentChild="Hire 5 salesman in a row." money={this.props.money} maximumSalesHirable={this.props.maximumSalesHirable} salesman={this.props.salesman}/>
+        <Button onClickProp={this.props.buyFiveSales} picture="./pictures/x5.png" name="Hire 5 salesmen" classNameButton="salesman-button menu-button investment-button" cost={this.props.salesmanCost *5 } classNameChild="salesman-machines-button" contentChild="Hire 5 salesman in a row." money={this.props.money} maximumSalesHirable={this.props.maximumSalesHirable} salesman={this.props.salesman}/>
     : null}
 
     {this.props.officeLevelUpgrade > 1 ?
-    <Button onClickProp={this.props.hireAManager} name="Hire a Manager" picture="./pictures/manager.png" classNameButton="rd-hire-manager menu-button" cost={this.props.managerCost} classNameChild="info-hire-manager" contentChild="Hire a Manager (hires 1 sales every second)" money={this.props.money} maximumSalesHirable={this.props.maximumSalesHirable} salesman={this.props.salesman}/> 
+    <Button onClickProp={this.props.hireAManager} name="Hire a Manager" picture="./pictures/manager.png" classNameButton="rd-hire-manager menu-button investment-button" cost={this.props.managerCost} classNameChild="info-hire-manager" contentChild="Hire a Manager (hires 1 sales every second)" money={this.props.money} maximumSalesHirable={this.props.maximumSalesHirable} salesman={this.props.salesman}/> 
     : null}
 
     {this.props.salesLevelOfInvestment > 4 &&
-     <div><h3>Research & Development</h3>
+     <div><h3>RESEARCH & DEVELOPMENT</h3>
         <div><Button onClickProp={this.props.investRD} name="Invest in Research" picture="./pictures/invest-in-research-and-development.png" classNameButton={classNameRDButton} cost={this.props.rdCost} classNameChild="info-invest-rd-button" contentChild="Unlock new possibilities." money={this.props.money}/>
-        {this.props.rdLevelOfInvestment > 4 && <Button onClickProp={this.props.buyOffice} name="Buy new offices" picture="./pictures/new_office.png" classNameButton="office-button menu-button" cost={this.props.officeCost} classNameChild="info-buy-office-button" contentChild="Hire more sales" money={this.props.money} />}
+        {this.props.rdLevelOfInvestment > 4 && <Button onClickProp={this.props.buyOffice} name="Buy new offices" picture="./pictures/new_office.png" classNameButton="office-button menu-button investment-button" cost={this.props.officeCost} classNameChild="info-buy-office-button" contentChild="Hire more sales" money={this.props.money} />}
     
 
     {this.props.rdLevelOfInvestment > 5 && this.props.softwareLevelOfInvestment < 1 ?
-    <Button onClickProp={this.props.investInSoftware} name="Install Software" picture="./pictures/software-program.png" classNameButton="rd-install-software menu-button" cost={this.props.softwareCost} classNameChild="info-invest-rd-install-software" contentChild="With software, we will be able to manage all this production. (+2000 sales)" money={this.props.money}/>
+    <Button onClickProp={this.props.investInSoftware} name="Install Software" picture="./pictures/software-program.png" classNameButton="rd-install-software menu-button investment-button" cost={this.props.softwareCost} classNameChild="info-invest-rd-install-software" contentChild="With software, we will be able to manage all this production. (+2000 sales)" money={this.props.money}/>
         :null}  
     </div></div>}
 
 {this.props.rdLevelOfInvestment > 4
     ? <div><h3>EXPANSION</h3><div>
-    <ButtonAutomaticData onClickProp={this.props.buyAMachine} money={this.props.money} name="Factory" picture="./pictures/factory.png" classNameButton="factory-button menu-button" cost={this.props.factoryCost} classNameChild="info-buy-factory-button" contentChild=" paperclips per second)" isManual={false} machineType="factory" machineProductivity={10000} machineQuantity={1} automaticProductivityPerMachine={this.props.productivyPerAutomaticMachine}/></div></div>
+    <ButtonAutomaticData onClickProp={this.props.buyAMachine} money={this.props.money} name="Factory" picture="./pictures/factory.png" classNameButton="factory-button menu-button investment-button" cost={this.props.factoryCost} classNameChild="info-buy-factory-button" contentChild=" paperclips per second)" isManual={false} machineType="factory" machineProductivity={10000} machineQuantity={1} automaticProductivityPerMachine={this.props.productivyPerAutomaticMachine}/></div></div>
     : null}
     </div>
         </div>}
      
 
         {this.props.cloudConnectionEstablished && <div className={className}>
-            <h2>EXECUTION</h2>
+            <h2 className="AI-context">EXECUTION</h2>
             <div className="padding-div">
 
             <div className={weightEarth}>
@@ -358,8 +358,8 @@ class InvestmentBox extends Component{
         <div>
             <Button onClickProp={this.props.investInAI} name="Find News Ways to make paperclips." picture="./pictures/findNewsWaysOfMakingPaperclips.png" classNameButton={classFindNewsWaysOfMakingPaperclipsDroneButton} cost={this.props.findNewsWayxToMakePaperclipCost} classNameChild="info-findNewsWays-button" contentChild="Goal : Produce the maximum amount of paperclips." money={this.props.money} machineType="find-news-ways-of-making-paperclips" machineQuantity={1} currency='computational'/>
             <Button onClickProp={this.props.investInAI} name="Create a Drone" picture="./pictures/drone.png" classNameButton={classNameOneDroneButton} cost={this.props.droneCost} classNameChild="info-drone-button" contentChild="This drone will ship our paperclips to customers. (+1000 sales per second)" money={this.props.money} machineType="drone" machineQuantity={1} currency='paperclips'/>
-            <Button onClickProp={this.props.investInAI} name="Create a Drone" picture="./pictures/50drones.png" classNameButton={classNameFiftyDroneButton} cost={this.props.droneCost*50} classNameChild="info-drone-button" contentChild="More drones." money={this.props.money} machineType="drone" machineQuantity={50} currency='paperclips'/>
-            <Button onClickProp={this.props.investInAI} name="Create a Drone" picture="./pictures/500drones.png" classNameButton={classNameFiveHundredDroneButton} cost={this.props.droneCost*500} classNameChild="info-drone-button" contentChild="More drones." money={this.props.money} machineType="drone" machineQuantity={500} currency='paperclips'/>
+            <Button onClickProp={this.props.investInAI} name="Create a Drone" picture="./pictures/x50.png" classNameButton={classNameFiftyDroneButton} cost={this.props.droneCost*50} classNameChild="info-drone-button" contentChild="More drones." money={this.props.money} machineType="drone" machineQuantity={50} currency='paperclips'/>
+            <Button onClickProp={this.props.investInAI} name="Create a Drone" picture="./pictures/x500.png" classNameButton={classNameFiveHundredDroneButton} cost={this.props.droneCost*500} classNameChild="info-drone-button" contentChild="More drones." money={this.props.money} machineType="drone" machineQuantity={500} currency='paperclips'/>
         </div>
         <div>
              <Button onClickProp={this.props.investInAI} name="Improve Drone" picture="./pictures/improveDrones.png" classNameButton={classNameImproveDroneButton} cost={this.props.ImprovedroneCost} classNameChild="info-improve-drone-button" contentChild="Increase Computational Power of the drones." money={this.props.money} machineType="improve-drone" currency='computational'/>
@@ -369,8 +369,8 @@ class InvestmentBox extends Component{
         </div>
         <div>
             <ButtonAutomaticData onClickProp={this.props.buyAMachine} money={this.props.money} name="Factory" picture="./pictures/factory.png" classNameButton={classNameFactoryButton} cost={10000} classNameChild="info-buy-factory-button" contentChild=" paperclips per second)" isManual={false} machineType="factory" machineProductivity={10000} machineQuantity={1} automaticProductivityPerMachine={this.props.productivyPerAutomaticMachine} currency='paperclips'/>
-            <ButtonAutomaticData onClickProp={this.props.buyAMachine} money={this.props.money} name="Factory" picture="./pictures/50factory.png" classNameButton={className50FactoryButton} cost={500000} classNameChild="info-buy-factory-button" contentChild=" paperclips per second)" isManual={false} machineType="factory" machineProductivity={10000} machineQuantity={50} automaticProductivityPerMachine={this.props.productivyPerAutomaticMachine} currency='paperclips'/>
-            <ButtonAutomaticData onClickProp={this.props.buyAMachine} money={this.props.money} name="Factory" picture="./pictures/500factory.png" classNameButton={className500FactoryButton} cost={5000000} classNameChild="info-buy-factory-button" contentChild=" paperclips per second)" isManual={false} machineType="factory" machineProductivity={10000} machineQuantity={500} automaticProductivityPerMachine={this.props.productivyPerAutomaticMachine} currency='paperclips'/>
+            <ButtonAutomaticData onClickProp={this.props.buyAMachine} money={this.props.money} name="Factory" picture="./pictures/x50.png" classNameButton={className50FactoryButton} cost={500000} classNameChild="info-buy-factory-button" contentChild=" paperclips per second)" isManual={false} machineType="factory" machineProductivity={10000} machineQuantity={50} automaticProductivityPerMachine={this.props.productivyPerAutomaticMachine} currency='paperclips'/>
+            <ButtonAutomaticData onClickProp={this.props.buyAMachine} money={this.props.money} name="Factory" picture="./pictures/x500.png" classNameButton={className500FactoryButton} cost={5000000} classNameChild="info-buy-factory-button" contentChild=" paperclips per second)" isManual={false} machineType="factory" machineProductivity={10000} machineQuantity={500} automaticProductivityPerMachine={this.props.productivyPerAutomaticMachine} currency='paperclips'/>
         </div>
         <div>
             <Button onClickProp={this.props.investInAI} name="Cure Cancer for Humanity" picture="./pictures/cureCancer.png" classNameButton={classNameCancerCuredButton} cost={this.props.cureCancerCost} classNameChild="info-cure-cancer-button" contentChild="Heal every cancer existing on Earth, for free." money={this.props.money} machineType="cure-cancer" currency='computational'/>
